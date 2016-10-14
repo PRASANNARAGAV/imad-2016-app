105035cdf381ff5fdb6ee421764952660279aa01
PRASANNARAGAV <vayujeevotamaha29@gmail.com>
2016-10-14 15:55:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+//Counter code
+var button=document.getElementbyId('counter');
+var counter=0;
+button.onclick=function(){
+  counter=counter+1;
+  var span=document.getElementbyId('count');
+  span.inner.HTML=counter.toString();
+    
+};
