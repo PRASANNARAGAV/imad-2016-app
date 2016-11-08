5b512b0276448c051cf2b3b45cae50c9aa0a9f25
PRASANNARAGAV <vayujeevotamaha29@gmail.com>
2016-11-08 19:49:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config ={
     database:'prasannaragav',
     host:'http://db.imad.hasura-app.io/',
     port: '5432',
-    password: process.emv.DB_PASSWORD,
+    password: 'process.emv.DB_PASSWORD',
 
     };
 
