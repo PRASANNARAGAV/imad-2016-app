1df04d1d7f9a75a088d21177b4cf5d9c95dcc501
PRASANNARAGAV <vayujeevotamaha29@gmail.com>
2016-11-26 18:24:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/counter',function (req,res) {
    app.get('/article/:articleName', function(req,res){
     
     articleName=req.paramas.articleName;
-    pool.query("SELECT * FROM article WHERE title= '"+ req.paramas.articleName +"'",function(err,result){
+    pool.query("SELECT * FROM  articles WHERE Title='",+ req.paramas.articleName +"'",function(err,result){
         if(err) {
             
         res.status(500).send(err.toString());
