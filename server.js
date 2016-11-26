5082ed31ca67ac03c8c6655e46562c0f5d6dd0db
PRASANNARAGAV <vayujeevotamaha29@gmail.com>
2016-11-26 18:50:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,6 +31,45 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+
+
+function createTemplate (data){
+    var title= data.title;
+    var date= data.date;
+    var heading= data.heading;
+    var content=data.content;
+    
+    var htmlTemplate = `
+    <html>
+     <head>
+      <title>
+        ${title}
+         </title>
+         <meta name="viewport" content "width-device-width,initial-scale-1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+     
+     <div class= "container" >
+     <div> <a href=/>HOME</a
+     </div>
+     <hr/>
+     <h3>${heading}</h3>
+     <div>
+     ${date.toDateString()}</div>
+     
+     <div>${content}
+     </div>
+     </div>
+     </body>
+     </html>
+     ` ;
+     
+     return htmlTemplate;
+     }
+
+
 var pool = new Pool(config);
 app.get('/test-db', function(req,res) {
     //make a select request and return response with results
@@ -82,9 +121,10 @@ app.get('/counter',function (req,res) {
         
     });
     
+    
    
 });
-     
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 
