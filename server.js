3d5b7c06b3edf690763516598a4fd090a83f3ae1
PRASANNARAGAV <vayujeevotamaha29@gmail.com>
2017-01-07 20:30:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ function createTemplate (data){
 var pool = new Pool(config);
 app.get('/test-db', function(req,res) {
     //make a select request and return response with results
-    pool.query('SELECT * FROM "student" WHERE "dept"=\'mech', function(err, result){
+    pool.query('SELECT * FROM "student" WHERE "dept"= "mech" ', function(err, result){
        if (err)
        {
            res.status(500).send(err.toString());
